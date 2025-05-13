8c852e1938df96ab2ead44ad21f2ab25bac60469
blqies20 <balqis1maharani@gmail.com>
2025-05-13 11:21:29 +0700

    memanggil dengan function chain

--- chain_fucntion_call.cpp
+++ chain_fucntion_call.cpp
@@ -18,4 +18,9 @@ class Buku
 };
 
 int main()
-{}
+{
+    Buku bukunya;
+    cout << bukunya.setJudul("Matematika").getJudul();
+    cout << bukunya.setJudul("Sains").getJudul();
+    cout << bukunya.setJudul("English").getJudul();
+}
